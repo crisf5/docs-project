454af4864015e3dacfa2849f0b99d5e5a95215b5
crisf5 <cristianfcba@gmail.com>
2023-01-17 19:20:36 -0300

    add toHex in shautils

--- src/main/java/com/documentcba/doc/utils/SHAUtil.java
+++ src/main/java/com/documentcba/doc/utils/SHAUtil.java
@@ -13,12 +13,22 @@ public class SHAUtil {
         return (hashType.equalsIgnoreCase("SHA-256") || hashType.equalsIgnoreCase("SHA-512"));
     }
 
+    public static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
     public static String toSHA256String(MultipartFile file) throws NoSuchAlgorithmException, IOException  {
 
         MessageDigest SHA256Digest = MessageDigest.getInstance("SHA-256");
         byte[] messageDigest = SHA256Digest.digest(file.getBytes());
 
-        return messageDigest.toString();
+        String toHex = bytesToHex(messageDigest);
+
+        return toHex;
     }
 
     public static String toSHA512String(MultipartFile file) throws  NoSuchAlgorithmException, IOException{
@@ -26,7 +36,9 @@ public class SHAUtil {
         MessageDigest SHA256Digest = MessageDigest.getInstance("SHA-512");
         byte[] messageDigest = SHA256Digest.digest(file.getBytes());
 
-        return messageDigest.toString();
+        String toHex = bytesToHex(messageDigest);
+
+        return toHex;
     }
 
 }
